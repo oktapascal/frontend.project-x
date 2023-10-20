d734c5104c8a20139f94ce4adc8d98faa32efe88
oktapascal <oktaiscool@gmail.com>
2023-10-20 10:12:56 +0700

    refactor main page

--- src/pages/main/Page.tsx
+++ src/pages/main/Page.tsx
@@ -1,67 +1,11 @@
-import axios from "axios";
-import { FC, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import { useLocalStorage } from "@/hooks";
-import {
-  useDisclosure,
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogContent,
-  AlertDialogOverlay,
-  Button,
-  Container,
-  Heading,
-  Text,
-} from "@chakra-ui/react";
-import { Navbar } from "./components";
+import { FC } from "react";
+import { useDisclosure, Container } from "@chakra-ui/react";
+import { Navbar, SignOutAlert } from "./components";
 
 import "./styles/styles.min.css";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
 const Page: FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const cancelRef = useRef<HTMLButtonElement>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const navigate = useNavigate();
-
-  const [, , removeSession] = useLocalStorage("session");
-
-  const [, , removeCookieSessionToken] = useCookies(["session-token"]);
-  const [, , removeCookieSessionRefreshToken] = useCookies([
-    "session-refresh-token",
-  ]);
-
-  const onSignOut = async () => {
-    setLoading(true);
-
-    const response = await axios.patch(
-      `${BASE_URL}/auth/logout`,
-      {},
-      {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    setLoading(false);
-
-    if (response.status === 200) {
-      removeSession("session");
-
-      removeCookieSessionToken("session-token");
-      removeCookieSessionRefreshToken("session-refresh-token");
-
-      navigate("/login");
-    }
-  };
 
   return (
     <>
@@ -72,42 +16,7 @@ const Page: FC = () => {
         </Container>
       </div>
 
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader>
-              <Heading as="h6" fontSize="1.25rem">
-                Keluar Aplikasi ?
-              </Heading>
-            </AlertDialogHeader>
-            <AlertDialogBody>
-              <Text>Semua halaman akses yang sama akan keluar.</Text>
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose} type="button">
-                Batal
-              </Button>
-              <Button
-                ref={cancelRef}
-                onClick={onSignOut}
-                type="button"
-                marginLeft="0.5rem"
-                backgroundColor="#0058e4"
-                color="#ffffff"
-                disabled={loading}
-                _hover={{ backgroundColor: "#004fcd" }}
-                _active={{ backgroundColor: "#0046b6" }}
-              >
-                Keluar
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
+      <SignOutAlert isOpen={isOpen} onClose={onClose} />
     </>
   );
 };
---
+++ src/pages/main/components/SignOutAlert.tsx
@@ -0,0 +1,105 @@
+import axios from "axios";
+import { FC, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { useLocalStorage } from "@/hooks";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const SignOutAlert: FC<Props> = ({ isOpen, onClose }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const cancelRef = useRef<HTMLButtonElement>(null);
+
+  const [, , removeSession] = useLocalStorage("session");
+
+  const [, , removeCookieSessionToken] = useCookies(["session-token"]);
+  const [, , removeCookieSessionRefreshToken] = useCookies([
+    "session-refresh-token",
+  ]);
+
+  const navigate = useNavigate();
+
+  const onSignOut = async () => {
+    setLoading(true);
+
+    const response = await axios.patch(
+      `${BASE_URL}/auth/logout`,
+      {},
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setLoading(false);
+
+    if (response.status === 200) {
+      removeSession("session");
+
+      removeCookieSessionToken("session-token");
+      removeCookieSessionRefreshToken("session-refresh-token");
+
+      navigate("/login");
+    }
+  };
+
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <Heading as="h6" fontSize="1.25rem">
+              Keluar Aplikasi ?
+            </Heading>
+          </AlertDialogHeader>
+          <AlertDialogBody>
+            <Text>Semua halaman akses yang sama akan keluar.</Text>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose} type="button">
+              Batal
+            </Button>
+            <Button
+              ref={cancelRef}
+              onClick={onSignOut}
+              type="button"
+              marginLeft="0.5rem"
+              backgroundColor="#0058e4"
+              color="#ffffff"
+              disabled={loading}
+              _hover={{ backgroundColor: "#004fcd" }}
+              _active={{ backgroundColor: "#0046b6" }}
+            >
+              Keluar
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  );
+};
+
+export default SignOutAlert;
